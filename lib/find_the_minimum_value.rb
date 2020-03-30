183d0fbf38d3e23305300dc89e3a7bfdd6fa68ea
Steven Kandow <kando1sr@gmail.com>
2020-03-30 17:10:50 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,6 +1,6 @@
 def find_min_value(array)
-  min_value = 0 
-  count = 0 
+  min_value = array[0]
+  count = 1 
   while count < array.length do 
     if array[count] <= min_value 
       min_value = array[count]
